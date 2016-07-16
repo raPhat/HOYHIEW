c881d1b5a5cff73b737377114c18fd227f94b141
raPhat <karjkeng@hotmail.com>
2016-07-16 16:02:37 +0700

    add async-http

--- Application/app/src/main/java/th/co/hoyhiew/hoyhiewapplication/MainActivity.java
+++ Application/app/src/main/java/th/co/hoyhiew/hoyhiewapplication/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String jsonUrl="http://raykarj.com/menu.json";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
